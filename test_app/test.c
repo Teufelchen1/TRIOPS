af048eeda49315e15fab9aaf88f4cdd9eec308e5
Bennet Blischke <bennet.blischke@haw-hamburg.de>
2024-11-08 13:46:53 +0100

    ✨ Feat: Enable Stdio for --headless mode

--- test_app/test.c
+++ test_app/test.c
@@ -17,37 +17,30 @@ void main(void) {
     print("Hello world!\n");
     print("Type CAT for a fun time!\n");
     print("Type $ to exit.\n");
-    int exit = 1;
+    int exit = 0;
     int last_was_C = 0;
     int last_was_A = 0;
-    int last_was_T = 0;
     do {
         char i = read();
         if (i != 0)
             puts(i);
-        if(i == '$') exit = 0;
+        if(i == '$') exit = 1;
         if (i == 'C') {
             last_was_C = 1;
             last_was_A = 0;
-            last_was_T = 0;
         } else if (last_was_C) {
             last_was_C = 0;
             if (i == 'A') {
                 last_was_A = 1;
-                last_was_T = 0;
             }
         }
         else if (last_was_A) {
             last_was_A = 0;
             if (i == 'T') {
-                last_was_T = 1;
+                print("\n _._     _,-'\"\"`-._\n(,-.`._,'(       |\\`-/|\n    `-.-' \\ )-`( , o o)\n          `-    \\`_`\"'-\n");
             }
         }
-        else if (last_was_T) {
-            last_was_T = 0;
-            print(" _._     _,-'\"\"`-._\n(,-.`._,'(       |\\`-/|\n    `-.-' \\ )-`( , o o)\n          `-    \\`_`\"'-\n");
-        }
-    } while(exit != 0);
+    } while(exit == 0);
     puts('\n');
 
     /* Signal termination */
